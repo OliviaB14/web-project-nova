449eadd126673101cd64c3e7e70e872d902d5bac
nathan5580 <nathan5580@gmail.com>
2018-02-13 16:39:31 +0100

    ded
    dedd

--- novagest/resources/views/login.blade.php
+++ novagest/resources/views/login.blade.php
@@ -1,47 +1,18 @@
 <html><head>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
-    <style>
-        body {
-            display: flex;
-            min-height: 100vh;
-            flex-direction: column;
-        }
-        
-        main {
-            flex: 1 0 auto;
-        }
-        
-        body {
-            background: #fff;
-        }
-        
-        .input-field input[type=date]:focus+label,
-        .input-field input[type=text]:focus+label,
-        .input-field input[type=email]:focus+label,
-        .input-field input[type=password]:focus+label {
-            color: #e91e63;
-        }
-        
-        .input-field input[type=date]:focus,
-        .input-field input[type=text]:focus,
-        .input-field input[type=email]:focus,
-        .input-field input[type=password]:focus {
-            border-bottom: 2px solid #e91e63;
-            box-shadow: none;
-        }
-    </style>
+    <link href="{{ URL::asset('css/login.css') }}" rel="stylesheet" />
 </head>
-
 <body>
     <div class="section"></div>
     <main>
         <center>
+            
             <!--<img class="responsive-img" style="width: 250px;" src="https://i.imgur.com/ax0NCsK.gif" />-->
             <div class="section"></div>
-
-            <h5 class="indigo-text">Merci de vous connecter</h5>
-            <div class="section"></div>
+    <h1>NOVAGEST</h1>
+            <h5 class="blue-text">Merci de vous connecter</h5>
+            <div class="section"></div> 
 
             <div class="container">
                 <div class="z-depth-1 grey lighten-4 row" style="display: inline-block; padding: 32px 48px 0px 48px; border: 1px solid #EEE;">
@@ -72,7 +43,7 @@
                         <br>
                         <center>
                             <div class="row">
-                                <button type="submit" name="btn_login" class="col s12 btn btn-large waves-effect indigo">Connection</button>
+                                <button type="submit" name="btn_login" class="col s12 btn btn-large waves-effect blue">Connection</button>
                             </div>
                         </center>
                     </form>
