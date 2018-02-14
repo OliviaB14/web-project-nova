dfef0137df064d882c34f71969030ff0690cb81e
ADRIEN MARINI <adrien.marini@viacesi.fr>
2018-02-14 15:13:42 +0100

    ajout sur l'accueil

--- novagest/resources/views/welcome.blade.php
+++ novagest/resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
 <h1>Welcome to NovaGest</h1>
 <section>
 <!--<a class="waves-effect waves-light btn" href="{{ url('/login') }}">Se connecter</a>-->
+<iframe width="560" height="315" src="https://www.youtube.com/embed/tdk8vfKhoug" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 </section>
 @stop
 
