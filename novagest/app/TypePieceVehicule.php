adc2d114d64479b033d0c3953fdf4732932b4362
nathan5580 <nathan5580@gmail.com>
2018-02-14 14:49:58 +0100

    Ajout model
    Moi dieu

---
+++ novagest/app/TypePieceVehicule.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TypePieceVehicule extends Model
+{
+
+    protected $table = 'type_piecevehicule';
+
+    public $timestamps = false;
+}
