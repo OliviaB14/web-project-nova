c86ae08bee4168d757447324bb8826a8c8d25f5a
nathan5580 <nathan5580@gmail.com>
2018-02-26 16:47:43 +0100

    ez

--- novagest/resources/views/includes/footer.blade.php
+++ novagest/resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
 
-  <footer class="page-footer brown-text darken-4">
+  <footer class="page-footer brown-text darken-4"></footer>
     <div class="container">
       <div class="row">
         <div class="col l6 s12">
