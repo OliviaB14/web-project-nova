fa6ac000091a472508131adb7f784abad5e2ef0e
nathan5580 <nathan5580@gmail.com>
2018-02-26 14:37:59 +0100

    ez

--- novagest/resources/views/agence.blade.php
+++ novagest/resources/views/agence.blade.php
@@ -95,7 +95,7 @@
                 </div>
             </div>
             
-            {{ Form::submit('Ajouter', array('class' => 'btn-sm btn-success')) }}
+            {{ Form::submit('Ajouter', array('class' => 'waves-effect waves-light btn')) }}
         {{ Form::close() }}
         </div>
       </div>
