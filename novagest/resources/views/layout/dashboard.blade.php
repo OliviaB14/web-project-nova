7d6c2659929b22487243061402dc74b29d482d51
nathan5580 <nathan5580@gmail.com>
2018-02-26 17:09:37 +0100

    ez

--- novagest/resources/views/layout/dashboard.blade.php
+++ novagest/resources/views/layout/dashboard.blade.php
@@ -7,7 +7,7 @@
         <div class="section">
           <div class="col s12 m8 l10" id="dash-content"> <!-- page content -->
             @yield('content')
-            <div class="container center-align">
+            <div class="container center-align" style="margin-top:22%">
               <a class="btn btn-floating btn-large animated bounce" id="bottom-arrow"><i class="material-icons">keyboard_arrow_down</i></a>
               <i class="material-icons"></i>
             </div>
