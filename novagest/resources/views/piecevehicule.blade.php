06a1bc38273162244fa723706a69dab623157db8
ADRIEN MARINI <adrien.marini@viacesi.fr>
2018-02-26 14:43:49 +0100

    view piece vehicule
    view piece vehicule

---
+++ novagest/resources/views/piecevehicule.blade.php
@@ -0,0 +1,101 @@
+@extends('layout.dashboard')
+
+@section('css-links')
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/welcome.css')}}"/>
+    <style>
+        .card.horizontal {
+        /*display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;*/
+    }
+    </style>
+@stop   
+
+@section('title', 'Piece_vehicule')
+
+@section('content')
+
+
+<section>
+    <div class="container">
+        <div class="row">
+        {{ Form::open(array('url' => 'piece_vehicule/add')) }}
+            <div class="col s12">
+                <div class="row">
+                    <div class="input-field col s6">
+                        {{ Form::label('id', 'Id')}}
+                        {{ Form::text('id', null,array('class'=>'validate', 'required' => 'required'))}}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        {{ Form::label('date_entree', 'Date_entree')}}
+                        {{ Form::text('date_entree', null,array('class'=>'validate', 'required' => 'required'))}}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s6">
+                        {{ Form::label('typeetatpiece', 'Typeetatpiece')}}
+                        {{ Form::text('typeetatpiece', null,array('class'=>'validate', 'required' => 'required'))}}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s6">
+                        {{ Form::label('typepiece', 'Typepiece')}}
+                        {{ Form::text('typepiece', null,array('class'=>'validate', 'required' => 'required'))}}
+                    </div>
+                </div>
+            </div>
+            {{ Form::submit('Ajouter', array('class' => 'btn-sm btn-success')) }}
+        {{ Form::close() }}
+        </div>
+    </div>
+</section>
+
+
+<section>
+<div class="container">
+<div class="row" style="padding-top:15px">
+<!--<a class="btn btn-floating btn-large cyan pulse"><i class="material-icons">add</i></a>-->
+</div>
+        <div class="row" style="padding-top:10px">
+        <div>
+            <table id="example" class="mdl-data-table responsive-table" cellspacing="0" width="100%">
+                    <thead>
+                        <tr>
+                            <th>N°</th>
+                            <th>Date d'entrée</th>
+                            <th>Etat de la pièce</th>
+                            <th>Type de pièce</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($clients as $client)
+                        <tr>
+                            <td>{{$client->id}}</td>
+                            <td>{{$client->date_entree}}</td>
+                            <td>{{$client->idtypeetatpiece}}</td>
+                            <td>{{$client->idtypepiece}}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+        </div>
+    </div>
+</div>
+</section>
+
+<script>
+$(document).ready(function() {
+    $('#example').DataTable( {
+        columnDefs: [
+            {
+                targets: [ 0, 1],
+                className: 'mdl-data-table__cell--non-numeric'
+            }
+        ]
+    } );
+} );
+          
+</script>
+@stop
