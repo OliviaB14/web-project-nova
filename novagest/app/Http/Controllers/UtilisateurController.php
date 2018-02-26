44b553117907797c89702445010e8869d3672edf
ADRIEN MARINI <adrien.marini@viacesi.fr>
2018-02-26 09:29:02 +0100

    objet
    controller utilisateur fait

--- novagest/app/Http/Controllers/UtilisateurController.php
+++ novagest/app/Http/Controllers/UtilisateurController.php
@@ -12,11 +12,67 @@ use Input;
 
 class UtilisateurController extends Controller
 {
-	public function GetUtilisateurs()
-	{
-		$utilisateurs = DB::table('utilisateur')
-        ->get();
-        //dd($utilisateurs);
-		return view('utilisateur', ['users' => $utilisateurs]);
-	}
+	// public function GetUtilisateurs()
+	// {
+	// 	$utilisateurs = DB::table('utilisateur')
+    //     ->get();
+    //     //dd($utilisateurs);
+	// 	return view('utilisateur', ['users' => $utilisateurs]);
+	// }
+
+	public function index()
+    {
+        $utilisateur = Utilisateur::all();
+        return $this->sendResponse(true, null, $utilisateur);
+    }
+
+    public function show($id)
+    {
+        $utilisateur = Utilisateur::find($id);
+        if ($utilisateur != null) {
+            return $this->sendResponse(true, null, $utilisateur);
+        }
+        return $this->sendResponse(false, "Data not found.", null);
+    }
+
+    public function update($id, Request $request)
+    {
+        // @TODO @Nathan please validate the data
+
+        // Find the corresponding record
+        $utilisateur = Utilisateur::find($id);
+        // Populate data
+        if ($utilisateur != null) {
+            $this->populateData($utilisateur, $request);
+            // Save
+            $utilisateur->save();
+            return $this->sendResponse(true, null, $utilisateur);
+        }
+        return $this->sendResponse(false, "Data not found.", null);
+    }
+
+    public function store(Request $request)
+    {
+        // @TODO @Nathan please validate the data
+
+        // Create a new utilisateur from request param
+        $utilisateur = new Utilisateur;
+        // Populate data
+        $this->populateData($agence, $request);
+        // Save
+        $utilisateur->save();
+        return $this->sendResponse(true, null, $utilisateur);
+    }
+
+    public function destroy($id)
+    {
+        // Find the corresponding record
+        $utilisateur = Utilisateur::find($id);
+        // Delete record
+        if ($utilisateur != null) {
+            $utilisateur->delete();
+            return $this->sendResponse(true, null, null);
+        }
+        return $this->sendResponse(false, "Data not found.", null);
+    }
 }
