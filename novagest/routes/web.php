721ddc6e31ffae7613564ab1e0cfd72a5da2d04e
nathan5580 <nathan5580@gmail.com>
2018-02-27 10:36:23 +0100

    ez

--- novagest/routes/web.php
+++ novagest/routes/web.php
@@ -7,26 +7,31 @@ Route::get('/logintest', function () {
     return view('login');
 });
 
-Route::get('/agences', function () {
-    return view('agence');
-});
-
-Route::get('/vehicules', function () {
-    return view('vehicules');
-});
-
 Route::get('/', 'AccueilController@index');
 
 // Routes avec controlleurs
 
 Route::get('villes', 'VilleController@GetVilles');
 Route::get('utilisateurs', 'UtilisateurController@GetUtilisateurs');
+
 //Agence
 Route::get('agences', 'AgenceController@index'); // index
 Route::post('agence/add', 'AgenceController@store'); // add
 Route::get('agences', 'AgenceController@index'); // show id
 //Fin agence
-
-Route::resources([
-    'agences' => 'AgenceController'
-]);
+Route::get('villes', 'VilleController@index');
+Route::get('utilisateurs', 'UtilisateurController@index');
+Route::get('agences', 'AgenceController@index');
+Route::get('clients', 'ClientController@index');
+Route::get('droits', 'DroitController@index');
+Route::get('droittypeutilisateurs', 'DroitTypeUtilisateursController@index');
+Route::get('historiquevehicule', 'HistoriqueVehiculeController@index');
+Route::get('piecevehicule', 'PieceVehiculeController@index');
+Route::get('statutvehicule', 'StatutVehiculeController@index');
+Route::get('typeclient', 'TypeClientController@index');
+Route::get('typeetatpiece', 'TypeEtatPieceController@index');
+Route::get('typehistoriqueevenement', 'TypeHistoriqueEvenementController@index');
+Route::get('typepiecevehicule', 'TypePieceVehiculeController@index');
+Route::get('typeutilisateur', 'TypeUtilisateurController@index');
+Route::get('typevehicule', 'TypeVehiculeController@index');
+Route::get('vehicule', 'VehiculeController@index');
