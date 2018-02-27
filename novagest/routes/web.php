2c9baf9e2d7f9aeba587df1f60754d8892a9e081
ADRIEN MARINI <adrien.marini@viacesi.fr>
2018-02-27 09:28:34 +0100

    maj route

--- novagest/routes/web.php
+++ novagest/routes/web.php
@@ -7,22 +7,27 @@ Route::get('/logintest', function () {
     return view('login');
 });
 
-Route::get('/agences', function () {
-    return view('agence');
-});
-
-Route::get('/vehicules', function () {
-    return view('vehicules');
-});
-
 Route::get('/', 'AccueilController@index');
 
 // Routes avec controlleurs
-
 Route::get('villes', 'VilleController@GetVilles');
 Route::get('utilisateurs', 'UtilisateurController@GetUtilisateurs');
 Route::get('agences', 'AgenceController@index');
+Route::get('clients', 'ClientController@GetClients');
+Route::get('droits', 'DroitController@GetDroits');
+Route::get('droittypeutilisateurs', 'DroitTypeUtilisateurs@GetDroitTypeUtilisateurs');
+Route::get('historiquevehicule', 'HistoriqueVehicule@GetHistoriqueVehicule');
+Route::get('piecevehicule', 'PieceVehicule@GetPieceVehicule');
+Route::get('statutvehicule', 'StatutVehicule@GetStatutVehicule');
+Route::get('typeclient', 'TypeClient@TypeClient');
+Route::get('typeetatpiece', 'TypeEtatPiece@TypeEtatPiece');
+Route::get('typehistoriqueevenement', 'TypeHistoriqueEvenement@GetTypeHistoriqueEvenement');
+Route::get('typepiecevehicule', 'TypePieceVehicule@GetTypePieceVehicule');
+Route::get('typeutilisateur', 'TypeUtilisateur@GetTypeUtilisateur');
+Route::get('typevehicule', 'TypeVehicule@GetTypeVehicule');
+Route::get('vehicule', 'Vehicule@GetVehicule');
 
+//vérifier utilité
 Route::resources([
     'agences' => 'AgenceController'
 ]);
