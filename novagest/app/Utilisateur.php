7dba5f32fb676a051bac6168f9def0944ec5b756
Olivia <olivia.boumokonia@laposte.net>
2018-03-22 14:54:23 +0100

    login

--- novagest/app/Utilisateur.php
+++ novagest/app/Utilisateur.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+/*namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -8,6 +8,21 @@ class Utilisateur extends Model
 {
 
     protected $table = 'utilisateur';
+
+    public $timestamps = false;
+}
+*/
+
+namespace App;
+
+
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+use Illuminate\Contracts\Auth\Authenticatable;
+
+class Utilisateur extends \Eloquent implements Authenticatable
+{
+	use AuthenticableTrait;
+	protected $table = 'utilisateur';
 
     public $timestamps = false;
 }
