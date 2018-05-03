3e5a506bf0f48ea0e807f8fa00c807f11f3d78c2
nathan5580 <nathan5580@gmail.com>
2018-05-03 17:20:26 +0200

    ez droits ville

--- novagest/resources/views/Ville.blade.php
+++ novagest/resources/views/Ville.blade.php
@@ -14,7 +14,7 @@
 @section('title', 'villes')
 
 @section('content')
-
+<?php $user = Auth::user();?>
 <div class="row">
     <div class="col s12"><h1><i class="material-icons">build</i> Gestion des villes</h1></div>
 </div>
@@ -65,6 +65,7 @@ $(document).ready(function(){
 
 <section>
 <ul class="collapsible" data-collapsible="accordion">
+@if(DB::table('droit_type_utilisateur')->where('idtypeutilisateur','=',$user->idtypeutilisateur)->where('iddroit','=',31)->exists())
     <li>
       <div class="collapsible-header"><i class="material-icons">add</i>Ajouter</div>
       <div class="collapsible-body">
@@ -99,6 +100,8 @@ $(document).ready(function(){
         </div>
       </div>
     </li>
+    @endif
+    @if(DB::table('droit_type_utilisateur')->where('idtypeutilisateur','=',$user->idtypeutilisateur)->where('iddroit','=',30)->exists())
     <li >
       <div class="collapsible-header"><i class="material-icons">dvr</i>Données</div>
       <div class="collapsible-body">
@@ -117,13 +120,21 @@ $(document).ready(function(){
                 <td>{{$task->id}}</td>
                 <td>{{$task->nom}}</td>
                 <td>{{$task->code_postal}}</td>
-                <td><a class="btn-floating btn-large waves-effect waves-light red" href="ville/destroy/{{$task->id}}"><i class="material-icons">cancel</i></a><a id="{{$task->id}}" class="btn-floating btn-large waves-effect waves-light yellow edit" href="#modal1"><i class="material-icons">edit</i></a></td>
+                <td>
+                @if(DB::table('droit_type_utilisateur')->where('idtypeutilisateur','=',$user->idtypeutilisateur)->where('iddroit','=',33)->exists())
+                <a class="btn-floating btn-large waves-effect waves-light red" href="ville/destroy/{{$task->id}}"><i class="material-icons">cancel</i></a>
+                @endif
+                @if(DB::table('droit_type_utilisateur')->where('idtypeutilisateur','=',$user->idtypeutilisateur)->where('iddroit','=',32)->exists())
+                <a id="{{$task->id}}" class="btn-floating btn-large waves-effect waves-light yellow edit" href="#modal1"><i class="material-icons">edit</i></a>
+                @endif
+                </td>
             </tr>
         @endforeach
         </tbody>
     </table>
       </div>
     </li>
+    @endif
   </ul>
 </section>
 
